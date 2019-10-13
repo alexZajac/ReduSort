8eb2627b2ed60966b133c2a11b05a38183cc1199
Alexandre Zajac <zajac.alexandre@gmail.com>
2019-10-13 12:51:27 -0700

    Updates

---
+++ precache-manifest.886c96e8dee0a1f1bbaa5aa6c61df26d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0dd4d1a3d46ab1d1d545f04ce8eacb7",
+    "url": "/sorting_visualization/index.html"
+  },
+  {
+    "revision": "88887a4758d51df9958e",
+    "url": "/sorting_visualization/static/css/main.609c87f7.chunk.css"
+  },
+  {
+    "revision": "109cde45c6e661a97f27",
+    "url": "/sorting_visualization/static/js/2.d602f9b8.chunk.js"
+  },
+  {
+    "revision": "88887a4758d51df9958e",
+    "url": "/sorting_visualization/static/js/main.8165e05e.chunk.js"
+  },
+  {
+    "revision": "0a0b35ee6aef31526265",
+    "url": "/sorting_visualization/static/js/runtime~main.af4b3d01.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sorting_visualization/precache-manifest.886c96e8dee0a1f1bbaa5aa6c61df26d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sorting_visualization/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
